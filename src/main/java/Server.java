03f4606a8c71d93be57f332d0176915d6a79fb96
Milhail <vip.moneta95@mail.ru>
2020-02-24 22:56:38 +0700

    Server working

---
+++ src/main/java/Server.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        ServerSocket serverSocket = new ServerSocket(8000);
+
+        while (true){
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client accepted " + (++count));
+            OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+            writer.write("HTTP/1.0 200 OK\n + " +
+                    "Content-type: text/html\n" +
+                    "\n" +
+                    "<h1>Hello JAVA " + count + "</h1>\n");
+            writer.flush();
+            writer.close();
+
+            clientSocket.close();
+        }
+        //serverSocket.close();
+    }
+}
